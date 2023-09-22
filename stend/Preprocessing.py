d8995ea95ea920ad1057a8756358a30ef3c94e96
ali <ali_ramazanov_2000@mail.ru>
2023-09-22 20:58:00 +0300

    refactor

--- stend/Preprocessing.py
+++ stend/Preprocessing.py
@@ -24,13 +24,40 @@ class DataProcessing:
     def __init__(self,path:str,name_protocol:str):
         self.path = path
         self.name_protocol = name_protocol
+        self.path_log = self.__path_log()
+        self.path_log_new =self.__path_log_new()
+        self.paths = self.__sort_in_folder()
+
+
+    def __path_log(self):
+        logs = glob.glob(os.path.join(self.path,'*.log'))
+        for val in logs:
+            if not re.search(f"(new|{self.name_protocol})", val):
+                return val
+
+        # print(logs)
+    def __path_log_new(self):
+        log_file = list(os.path.split(self.path_log))
+        # log_name = log_file[1].split('.')
+        # log_name[0]= log_name[0] +'_new'
+        # path_log_new = '.'.join(log_name)
+        # new_path_log = os.path.join(log_file[0],path_log_new)
+        log_file[1] = re.sub(r'(.+).(log)', r'\1_new.\2', log_file[1])
+        new_path_log = os.path.join(log_file[0],log_file[1])
+        return new_path_log
+
 
     def __sort_by(self,file:str) -> int:
         '''
         функция для сортировки при считывания файлов
         '''
         # return int(re.search(r'\d{3,}', file)[0])
-        return int(file.split('/')[-1].split('.')[0])
+        # os.path.split(self.path)
+        # return int(file.split('/')[-1].split('.')[0])
+        # os.path.split(file)[1].split('.')[0])
+        path = os.path.split(file)[1]
+        name_file = re.sub(r'([\d+\.]?\d+).json|([\d+\.]?\d+).npy', r'\1\2', path)
+        return float(name_file)
 
     def __sort_in_folder(self):
         '''
@@ -38,7 +65,6 @@ class DataProcessing:
         '''
 
         paths = []  # add paths all to folders
-
         extensions = {
             "npy": "Frame",
             "json": "Indication",
@@ -47,17 +73,18 @@ class DataProcessing:
         }
 
         for extension, folder_image in extensions.items():
-            files = glob.glob(os.path.join(self.path, fr"*.{extension}"))
+            files = glob.glob(os.path.join(self.path, f"*.{extension}"))
             print(f"[*] Найдено {len(files)} Файлов с раширением {extension}.")
             if not os.path.isdir(os.path.join(self.path, folder_image)):
-                # print(folder_image)
                 os.mkdir(os.path.join(self.path, folder_image))
                 print(f"[+] Создана папка {folder_image}.")
             folder_location = os.path.join(self.path, folder_image)
             paths.append(folder_location)
 
             for file in files:
-                if re.fullmatch(f'.*\d*\.\d*\.{extension}|.*exp_params.json', file) is not None:
+                # if 'exp_params'in file:
+                #     continue
+                if re.fullmatch(f'.*\d+\.?\d+?.{extension}|(?!.*exp_params.json)', file) is not None:
                     nowlocation = os.path.basename(file)
                     dst = os.path.join(self.path, folder_image, nowlocation)
                     print(f"[*] Перенесен файл '{file}' в {dst}")
@@ -67,52 +94,35 @@ class DataProcessing:
 
     def read_tenzo_value_log_to_dictionary(self):
         self.tenzo = {}
-        self.tenzo_time = []
+        self.sampling = []
         self.tenzo_data = []
-        with open(f'{self.path}') as datalog:
+        sum_sampling = 0
+        file_lines = 0
+        with open(f'{self.path_log}') as datalog:
             iter = 1
+            self.tenzo_data.append({0: [], 1: [], 2: [], 3: []})
             for index, value in enumerate(datalog):
-                self.tenzo_data.append({0: [], 1: [], 2: [], 3: []})
                 data = value.split(',')
-                # print(print(f'{motion=}'))
-                # gg = []
-                # for i in motion:
-                #     res = re.findall(r'[-+]?(?:\d+(?:\.\d*)?|\.\d+)', i)
-                #     gg.append(res[0])
-                # self.move.append(gg)
-                # self.tenzo_time.append(data[0][1:])
-                # print(f'{data=}')
-                print('=================================================')
                 for i,val in enumerate (data[-4:]):
-
                     res = re.findall(r'-?\d+', val)
-                    if int(data[1]) not in self.tenzo:
-                        self.tenzo[int(data[1])] = []
                     if int(data[1]) == iter:
-                        self.tenzo[iter].append(*res)
+                        self.tenzo_data[iter-1][i].append(float(*res))
                     else:
+                        #блок для вычисления дискретизации
+                        if len(self.sampling) == 0:
+                            self.sampling.append(index)
+                        else:
+                            sum_sampling += self.sampling[-1]
+                            self.sampling.append(index - sum_sampling)
                         iter += 1
+
+                        self.tenzo_data.append({0: [], 1: [], 2: [], 3: []})
                         if int(data[1]) == iter:
-                            self.tenzo[iter].append(*res)
-                    # print(f'{res=}')
+                            self.tenzo_data[iter-1][i].append(float(*res))
+                file_lines = index
             datalog.close()
-        # print(self.tenzo_time)
-    def parsing_tenzo_value(self):
-        self.sampling =[]
-        self.tenzo_data = []
-        for index, data in enumerate(self.tenzo):
-            self.tenzo_data.append({0:[],1:[],2:[],3:[]})
-            sampling_i = int(len(self.tenzo[data]) / 4)
-            self.sampling.append(sampling_i)
-            i_0 = 0
-            i_1 = 1
-            i_2 = 2
-            i_3 = 3
-            for i in range(sampling_i):
-                self.tenzo_data[index][0].append(abs(float(self.tenzo[data][i_0 + i * 4])))
-                self.tenzo_data[index][1].append(abs(float(self.tenzo[data][i_1 + i * 4])))
-                self.tenzo_data[index][2].append(abs(float(self.tenzo[data][i_2 + i * 4])))
-                self.tenzo_data[index][3].append(abs(float(self.tenzo[data][i_3 + i * 4])))
+        sum_sampling += self.sampling[-1]
+        self.sampling.append(file_lines - sum_sampling)
 
     @staticmethod
     def find_outliers_iqr(data):
@@ -122,8 +132,6 @@ class DataProcessing:
         lower_bound = q1 - 3 * iqr
         upper_bound = q3 + 3 * iqr
         for i, x in enumerate(data):
-            # if x == 2106498.0:
-            #     print(f'====================================================={x=}')
             #     print(f'{data=}\n,{lower_bound=},{upper_bound=}')
             if x < lower_bound or x > upper_bound:
                 # print(x)
@@ -134,18 +142,12 @@ class DataProcessing:
     def find_outliers_z_score(data):
         mean = np.mean(data)
         std = np.std(data)
-        # print(f'{std=}')
         lower_bound = mean - 3 * std
         upper_bound = mean + 3 * std
         for i,x in enumerate(data):
-            # if x == 2106498.0:
-            #     print(f'====================================================={x=}')
-            #
-            #     z_score = (x - mean) / std
             #     print(f'{data=}\n{z_score=},{lower_bound=},{upper_bound=},{mean=},{std=}')
             z_score = (x - mean) / std
             if z_score < lower_bound or z_score > upper_bound:
-                # print(x)
                 data.pop(i)
         return data
 
@@ -154,33 +156,31 @@ class DataProcessing:
             for key in window.keys():
                 values = window[key]
                 if len(values) > 3:
-                    # print('до',len(values))
                     # values = DataProcessing.find_outliers_z_score(list(set(values)))
                     values = DataProcessing.find_outliers_iqr(list(set(values)))
                     # print(f'{values=},\t std = {np.std(values)},\t len = {len(values)}\n')
                 window[key] = values
 
     def mean_value_tenzo(self):
-        self.json_files=glob.glob('stend/VHB491012mmBiostand1-3/Indication/*.json')
+        path_Indication = self.paths[1]
+        self.json_files=glob.glob(os.path.join(path_Indication,'*.json'))
         # coef = [46745, 45731, 45731, 39700]
         self.json_files.sort(key=self.__sort_by)
         # print(self.json_files)
         self.json_files_average_strength = {}
         # print(self.tenzo_data)
-        self.error_open = []
+        self.error_open = {}
         for index, window in enumerate(self.tenzo_data):
             with open(f'{self.json_files[index]}' ) as json_file:
-                file = eval(json.load(json_file))
-                # print(file)
+
                 try:
-                    pass
-                    # print(file.keys())
-                    file[f'{index+1}']
-                    # # print(self.file[f'{index + 1}'][1])
-                except KeyError:
-                    # print('==================================================')
-                    self.error_open.append(index+1)
-                    # print('==================================================')
+                    file = eval(json.load(json_file))
+                    file[f'{index + 1}']
+                except Exception as e:
+                    print(self.json_files[index])
+                    print(file)
+                    print(index+1)
+                    self.error_open[index+1] = e
                     continue
                 tenzo_mean_value = []
                 for key in window.keys():
@@ -197,17 +197,57 @@ class DataProcessing:
                 # print(f'{tenzo_mean_value=}')
 
     def rewriting_file_json(self):
+        print(self.error_open)
         for index, file in enumerate(self.json_files):
             with open(f'{file}','w') as json_file:
                 if index+1 in self.error_open:
-                    json.dump(f'{index+1:"Reading error"}',json_file )
+                    d = {index+1: f'Reading error  {self.error_open[index+1]}'}
+                    ff = json.dump(f'{d}',json_file)
+                    print(ff)
+                    break
+                    # print(json.dumps(f"{index + 1}:'Reading error  {self.error_open[index + 1]}'"))
+
                 else:
                     values=self.json_files_average_strength[f'{index + 1}']
-                    json.dump(f'{index + 1:{values}}',json_file)
+                    d = {f'{index + 1}':values}
+                    json.dump(f'{d}',json_file)
+                    # print(json.dumps(f"{index + 1}:{values}",indent=4))
 
     def rewriting_file_log(self):
-        pass
+        with open(f'{self.path_log}') as datalog:
+            with open(f'{self.path_log_new}','w', encoding='utf-8') as new_datalog:
+                iter = 1
+                for index, value in enumerate(datalog):
+                    data = value.split(',')
+                    move_1_3 = np.array([])
+                    move_2_4 =  np.array([])
+                    for i, val in enumerate(data[2:6]):
+                        res = re.findall(r'[-+]?(?:\d+(?:\.\d*)?|\.\d+)', val)
+                        if i == 0 or i == 1:
+                            move_1_3 = np.append(move_1_3,float(*res))
+                        elif i == 2 or i == 3:
+                            move_2_4 = np.append(move_2_4,float(*res))
+                    print(int(data[1]), iter)
+                    if int(data[1]) == iter:
+                        move_1_3 = move_1_3 / self.sampling[iter-1]
+                        move_2_4 = move_2_4 / self.sampling[iter-1]
+                    else:
+                        iter += 1
+                        if int(data[1]) == iter:
+                            move_1_3 = move_1_3 / self.sampling[iter - 1]
+                            move_2_4 = move_2_4 / self.sampling[iter - 1]
+                        else:
+                            print('dddddddd')
+                    data_str_datalog = [tuple(move_1_3),tuple(move_2_4)]
+                    s = datalog.readline()
+                    res = re.sub(r'\(\([-+]?\d+\.\d+, [-+]?\d+\.\d+\), \([-+]?\d+\.\d+, [-+]?\d+\.\d+\)\)', f'({data_str_datalog[0]}, {data_str_datalog[1]})',s)
+                    new_datalog.write(res)
+                    # print(s)
 
+                # res = re.search(r'\(\(.+, +), \(-\d+\.\d+, -\d+\.\d\)\)',s)
+                # print(res)
+            datalog.close()
+            new_datalog.close()
 
     # def drow_plot(self):
     #     sss = []
@@ -217,12 +257,13 @@ class DataProcessing:
     #     plt.plot(sss)
     #     plt.show()
 def test():
-    tt = DataProcessing('stend/VHB491012mmBiostand1-3/exp_params.log','ttt')
-    tt.read_tenzo_value_log_to_dictionary()
-    tt.parsing_tenzo_value()
+    # tt = DataProcessing('confa','ttt')
+    tt = DataProcessing('stend/VHB491012mmBiostand1-3','ttt')
+    # tt.read_tenzo_value_log_to_dictionary()
     # tt.search_outlier()
     # tt.mean_value_tenzo()
     # tt.rewriting_file_json()
+    # tt.rewriting_file_log()
     # tt.drow_plot()
 
 if __name__ == "__main__":
